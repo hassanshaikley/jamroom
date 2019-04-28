a141ab2991d33d15d9171f3056a850188d0ad398
Hassan Shaikley <hassan.shaikley@gmail.com>
2019-04-28 02:04:47 -0700

    Added howler for playing audio

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,12 +12,20 @@ import css from "../css/app.css"
 import "phoenix_html"
 
 // Import local files
+import { Howl, Howler } from 'howler';
+
+
+var guitarSound = new Howl({
+    src: ['sounds/a_maj_guitar.mp3']
+});
+
 //
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
 
 window.playGuitar = ({ chord, stroke }) => {
     console.log(`Playing ${chord} in a ${stroke}-stroke`)
+    guitarSound.play()
 }
 
 import LiveSocket from "phoenix_live_view"
