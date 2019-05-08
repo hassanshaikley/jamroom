8ca7b36a06d2dba3bc4fb0f2db328dd77b45be82
Hassan Shaikley <hassan.shaikley@gmail.com>
2019-05-08 10:30:55 -0700

    Refactor

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,9 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
 import css from "../css/app.css"
 
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
 import "phoenix_html"
+import LiveSocket from "phoenix_live_view"
 
-// Import local files
-import { Howl, Howler } from 'howler';
-
-
-const guitarSounds = {
-    a: new Howl({
-        src: ['sounds/ax-grinder__a2-pmute.wav']
-    }),
-    b: new Howl({
-        src: ['sounds/ax-grinder__b2-pmute.wav']
-    }),
-    c: new Howl({
-        src: ['sounds/ax-grinder__c2-pmute.wav']
-    }),
-    d: new Howl({
-        src: ['sounds/ax-grinder__d2-pmute.wav']
-    }),
-    e: new Howl({
-        src: ['sounds/ax-grinder__e2-pmute.wav']
-    }),
-    f: new Howl({
-        src: ['sounds/ax-grinder__f2-pmute.wav']
-    }),
-    g: new Howl({
-        src: ['sounds/ax-grinder__g2-pmute.wav']
-    })
-}
-
-
-
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+import { guitarSounds, drumSounds } from './sounds'
 
 window.playGuitar = ({ chord, stroke }) => {
     if (guitarSounds[chord]) {
@@ -54,31 +14,15 @@ window.playGuitar = ({ chord, stroke }) => {
     }
 }
 
-const drumSounds = {
-    "1": new Howl({
-        src: ['sounds/snare-drum.wav']
-    }),
-    "2": new Howl({
-        src: ['sounds/kick-drum.wav']
-    }),
-    "3": new Howl({
-        src: ['sounds/bass-drum.wav']
-    }),
-    "4": new Howl({
-        src: ['sounds/cymbal-drum.mp3']
-    }),
-}
-
 window.playDrum = ({ key }) => {
     drumSounds[key].play()
 }
 
-import LiveSocket from "phoenix_live_view"
+window.addEventListener("keydown", event => {
+    console.log("Keys disabled, heh, sry")
+    event.preventDefault()
+});
 
 let liveSocket = new LiveSocket("/live");
 
-
-
-
 liveSocket.connect();
-
---
+++ assets/js/sounds.js
@@ -0,0 +1,41 @@
+import { Howl, Howler } from 'howler';
+
+
+export const guitarSounds = {
+    "1": new Howl({
+        src: ['sounds/ax-grinder__a2-pmute.wav']
+    }),
+    "2": new Howl({
+        src: ['sounds/ax-grinder__b2-pmute.wav']
+    }),
+    "3": new Howl({
+        src: ['sounds/ax-grinder__c2-pmute.wav']
+    }),
+    "4": new Howl({
+        src: ['sounds/ax-grinder__d2-pmute.wav']
+    }),
+    "5": new Howl({
+        src: ['sounds/ax-grinder__e2-pmute.wav']
+    }),
+    "6": new Howl({
+        src: ['sounds/ax-grinder__f2-pmute.wav']
+    }),
+    "7": new Howl({
+        src: ['sounds/ax-grinder__g2-pmute.wav']
+    })
+}
+
+export const drumSounds = {
+    "1": new Howl({
+        src: ['sounds/snare-drum.wav']
+    }),
+    "2": new Howl({
+        src: ['sounds/kick-drum.wav']
+    }),
+    "3": new Howl({
+        src: ['sounds/bass-drum.wav']
+    }),
+    "4": new Howl({
+        src: ['sounds/cymbal-drum.mp3']
+    }),
+}
